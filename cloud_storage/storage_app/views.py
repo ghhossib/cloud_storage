1d13e59b4f19cac0bce9a8b4f6852fc578fb8571
python <memngr40@gmail.com>
2026-01-15 14:24:25 +0500

    d

--- cloud_storage/storage_app/views.py
+++ cloud_storage/storage_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from rest_framework import views
+from rest_framework.permissions import AllowAny
+
+
 # Create your views here.
 
-class LoginView(views.APIView)
+class LoginView(views.APIView):
+    permission_classes = [AllowAny]
